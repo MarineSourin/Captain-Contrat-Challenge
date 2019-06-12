e872097ea5c0e11d2cd3a86774d201cf35dc9a23
Marine Sourin <marinesourin@live.fr>
2019-06-12 13:47:47 +0200

    seed personnages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning Personnages"
+Personnage.destroy_all
+
+puts "Creating Personnages"
+
+puts "Creating Mario"
+mario = Personnage.new
+mario.nom = "Mario"
+mario.vie = (20..100).to_a.sample
+mario.attaque = (30..60).to_a.sample
+mario.save!
+puts "Mario created"
+
+puts "Creating Link"
+link = Personnage.new
+link.nom = "Link"
+link.vie = (20..100).to_a.sample
+link.attaque = (30..60).to_a.sample
+link.save!
+puts "Link created"
+
+puts "Creating Pikachu"
+pickachu = Personnage.new
+pickachu.nom = "Pikachu"
+pickachu.vie = (20..100).to_a.sample
+pickachu.attaque = (30..60).to_a.sample
+pickachu.save!
+puts "Pikachu created"
+
+puts "Creating Sonic"
+sonic = Personnage.new
+sonic.nom = "Sonic"
+sonic.vie = (20..100).to_a.sample
+sonic.attaque = (30..60).to_a.sample
+sonic.save!
+puts "Sonic created"
+
+puts "Creating Kirby"
+kirby = Personnage.new
+kirby.nom = "Kirby"
+kirby.vie = (20..100).to_a.sample
+kirby.attaque = (30..60).to_a.sample
+kirby.save!
+puts "Kirby created"
+
+puts "Creating Donkey Kong"
+donkey_kong = Personnage.new
+donkey_kong.nom = "Donkey Kong"
+donkey_kong.vie = (20..100).to_a.sample
+donkey_kong.attaque = (30..60).to_a.sample
+donkey_kong.save!
+puts "Donkey Kong created"
+
+puts "Finished"
