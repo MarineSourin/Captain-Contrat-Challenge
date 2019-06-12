58d7b315396757973055e49c43641db23630578e
Marine Sourin <marinesourin@live.fr>
2019-06-12 14:00:45 +0200

    show action + view + route

--- app/controllers/personnages_controller.rb
+++ app/controllers/personnages_controller.rb
@@ -1,5 +1,16 @@
 class PersonnagesController < ApplicationController
+  before_action :set_personnage, only: [:show]
+
   def index
     @personnages = Personnage.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_personnage
+    @personnage = Personnage.find(params[:id])
   end
 end
