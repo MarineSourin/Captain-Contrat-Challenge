48b72c098697815deeca3c53c7c0d25cfc1a4fab
Marine Sourin <marinesourin@live.fr>
2019-06-12 14:21:06 +0200

    new form + button index

--- app/controllers/personnages_controller.rb
+++ app/controllers/personnages_controller.rb
@@ -8,9 +8,26 @@ class PersonnagesController < ApplicationController
   def show
   end
 
+  def new
+    @personnage = Personnage.new
+  end
+
+  def create
+    @personnage = Personnage.new(personnage_params)
+    if @personnage.save
+      redirect_to personnage_path(@personnage)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_personnage
     @personnage = Personnage.find(params[:id])
+  end
+
+  def personnage_params
+    params.require(:personnage).permit(:nom, :vie, :attaque)
   end
 end
