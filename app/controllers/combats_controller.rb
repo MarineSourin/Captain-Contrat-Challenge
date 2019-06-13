87971c2b71c9d7847e18efb69555d1cece9fbd6b
Marine Sourin <marinesourin@live.fr>
2019-06-13 16:23:51 +0200

    almost done

--- app/controllers/combats_controller.rb
+++ app/controllers/combats_controller.rb
@@ -28,35 +28,33 @@ class CombatsController < ApplicationController
   def deroulement_combat(combat)
     joueurs = ordre_attaque(combat)
 
-    joueur1_vie = joueurs[:joueur1].personnage.vie
-    joueur2_vie = joueurs[:joueur2].personnage.vie
+    joueur_1_vie = joueurs[:joueur_1].personnage.vie
+    joueur_2_vie = joueurs[:joueur_2].personnage.vie
 
-    while joueur1_vie.positive?
-      joueur2_vie -= (joueurs[:joueur1].personnage.attaque / 2)
-      break
-      if joueur2_vie <= 0
-        joueur1_vie -= (joueurs[:joueur2].personnage.attaque / 2)
-      end
+    while joueur_1_vie.positive?
+      joueur_2_vie -= (joueurs[:joueur_1].personnage.attaque / 2)
+      break if joueur_2_vie <= 0
+      joueur_1_vie -= (joueurs[:joueur_2].personnage.attaque / 2)
     end
-    resultat(joueurs, joueur1_vie, joueur2_vie)
+    resultat(joueurs, joueur_1_vie, joueur_2_vie)
   end
 
   def ordre_attaque(combat)
     joueurs = combat.joueurs
-    joueur1 = joueurs.sample
-    joueur2 = joueurs.reject { |joueur| joueur == joueur1 }
-    return { joueur1: joueur1, joueur2: joueur2.first }
+    joueur_1 = joueurs.sample
+    joueur_2 = joueurs.reject { |joueur| joueur == joueur_1 }
+    return { joueur_1: joueur_1, joueur_2: joueur_2.first }
   end
 
-  def resultat(joueurs, joueur1_vie, joueur2_vie)
-    if joueur1_vie <= 0
-      joueurs[:joueur1].result = 'Perdant 👎🏻'
-      joueurs[:joueur2].result = 'Vainqueur 🏆'
-    elsif joueur2_vie <= 0
-      joueurs[:joueur1].result = 'Vainqueur 🏆'
-      joueurs[:joueur2].result = 'Perdant 👎🏻'
+  def resultat(joueurs, joueur_1_vie, joueur_2_vie)
+    if joueur_1_vie <= 0
+      joueurs[:joueur_1].result = 'Perdant 👎🏻'
+      joueurs[:joueur_2].result = 'Vainqueur 🏆'
+    elsif joueur_2_vie <= 0
+      joueurs[:joueur_1].result = 'Vainqueur 🏆'
+      joueurs[:joueur_2].result = 'Perdant 👎🏻'
     end
-    joueurs[:joueur1].save
-    joueurs[:joueur2].save
+    joueurs[:joueur_1].save
+    joueurs[:joueur_2].save
   end
 end
