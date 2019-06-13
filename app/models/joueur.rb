710d922af77d6f37286e56421e5449099e233340
Marine Sourin <marinesourin@live.fr>
2019-06-13 10:54:46 +0200

    validations & associations arme bouclier joueur

--- app/models/joueur.rb
+++ app/models/joueur.rb
@@ -1,4 +1,10 @@
 class Joueur < ApplicationRecord
   belongs_to :personnage
   belongs_to :combat, required: false
+  belongs_to :bouclier
+  belongs_to :arme
+
+  validate :personnage_id, presence: true
+  validate :bouclier_id, presence: true
+  validate :arme_id, presence: true
 end
